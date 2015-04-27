6b93aeac37c0e789c11566f5891723d36778d374
Marcel <marcel.pfleiderer@outlook.de>
2015-04-27 16:19:53 +0200

    scheiße raus

--- src/de/Database/TestClassGIT.java
+++
@@ -1,7 +0,0 @@
-package de.Database;
-
-/**
- * Created by Pascal on 27.04.2015.
- */
-public class TestClassGIT {
-}
--- src/de/Database/other/test.java
+++
@@ -1,7 +0,0 @@
-package de.Database.other;
-
-/**
- * Created by Marcel on 27.04.2015.
- */
-public class test {
-}
