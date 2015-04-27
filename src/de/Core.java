2fd9c0247e65a083d0b90ab9cbadac6174101419
mueckenputzer <mueckenputzer@outlook.com>
2015-04-27 17:00:48 +0200

    Hauptfenster erstellt

--- src/de/Core.java
+++ src/de/Core.java
@@ -1,11 +1,17 @@
 package de;
 
+import de.Window.MainFrame;
+
 /**
  * Created by Marcel on 20.04.2015.
  */
 public class Core {
 
     public static void main(String[] args) {
+
+        MainFrame myFrame = new MainFrame();
+
+        myFrame.
 
     }
 }
---
+++ src/de/Window/MainFrame.java
@@ -0,0 +1,23 @@
+package de.Window;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ * Created by Pascal on 27.04.2015.
+ */
+public class MainFrame {
+    private JTree tree1;
+    private JButton button1;
+
+    public MainFrame() {
+        button1.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+
+                System.out.println("Button gedrueckt!");
+
+            }
+        });
+    }
+}
