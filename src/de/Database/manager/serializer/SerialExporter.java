8545bc87825c880df8ec8b21c61324100d11a380
Marcel <marcel.pfleiderer@outlook.de>
2015-04-27 18:46:46 +0200

    refactor

--- src/de/Database/manager/serializer/SerialExporter.java
+++ src/de/Database/manager/serializer/SerialExporter.java
@@ -3,6 +3,11 @@ package de.database.manager.serializer;
 import de.database.DataKnot;
 import de.database.InOuter;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
 /**
  * Created by Marcel on 27.04.2015.
  */
@@ -10,11 +15,25 @@ public class SerialExporter extends InOuter {
 
     @Override
     public void write(String pFile, DataKnot pData) {
-
+        try {
+            ObjectOutputStream tempOut = new ObjectOutputStream(new FileOutputStream(pFile));
+            tempOut.writeObject(pData);
+            tempOut.close();
+        } catch(Exception ex) {
+            ex.printStackTrace();
+        }
     }
 
     @Override
     public DataKnot read(String pFile) {
+        try {
+            ObjectInputStream tempIn = new ObjectInputStream(new FileInputStream(pFile));
+            DataKnot tempKnot = (DataKnot) tempIn.readObject();
+            tempIn.close();
+            return tempKnot;
+        } catch(Exception ex) {
+            ex.printStackTrace();
+        }
         return null;
     }
 }
