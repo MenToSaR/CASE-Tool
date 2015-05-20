88baa5405593aa54165ac623e32533a405aa5a31
Pascal Betz <pascal.betz@outlook.com>
2015-05-20 13:29:51 +0200

    Kommentar added

---
+++ src/InfluenceFactors.java
@@ -0,0 +1,32 @@
+/**
+ * Created by Pascal_Betz on 20.05.15.
+ */
+
+import java.util.*
+
+public class InfluenceFactors {
+
+    HashMap<String, Integer> hmFactors = new HashMap<String, Integer>();
+
+    public InfluenceFactors(){
+
+        hmFactors.put("Datenkommunikation", new Integer(3));
+        hmFactors.put("Verteilte Verarbeitung", new Integer(3));
+        hmFactors.put("Leistungsanforderungen", new Integer(3));
+        hmFactors.put("Resourcennutzung", new Integer(3));
+        hmFactors.put("Transaktionsrate", new Integer(3));
+        hmFactors.put("Benutzerschnittstelle", new Integer(3));
+        hmFactors.put("Benutzerfreundlichkeit", new Integer(3));
+        hmFactors.put("Onlineverarbeitung", new Integer(3));
+        hmFactors.put("Komplexe Verarbeitung", new Integer(3));
+        hmFactors.put("Wiederverwendbarkeit", new Integer(3));
+        hmFactors.put("Migrationshilfen", new Integer(3));
+        hmFactors.put("Installationshilfen", new Integer(3));
+        hmFactors.put("Betriebshilfen", new Integer(3));
+        hmFactors.put("Aenderungsfreundlichkeit", new Integer(3));
+
+
+
+    }
+
+}
