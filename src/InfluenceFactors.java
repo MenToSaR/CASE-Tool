11fbe80ee816e93c30e46aeccf03d44258adf26e
Pascal Betz <pascal.betz@outlook.com>
2015-05-20 13:47:07 +0200

    InfluenceFactors

--- src/InfluenceFactors.java
+++ src/InfluenceFactors.java
@@ -5,26 +5,25 @@
 
 public class InfluenceFactors {
 
-    private int nFactors[] = new int[15];
+    private int nFactors[] = new int[14];
 
 
     public InfluenceFactors(){
 
-    nFactors[0] = 0;    //not used
-    nFactors[1] = 3;
+    nFactors[0] = 3;    //not used
+    nFactors[1] = 4;
     nFactors[2] = 4;
-    nFactors[3] = 4;
+    nFactors[3] = 2;
     nFactors[4] = 2;
-    nFactors[5] = 2;
-    nFactors[6] = 3;
-    nFactors[7] = 4;
+    nFactors[5] = 3;
+    nFactors[6] = 4;
+    nFactors[7] = 2;
     nFactors[8] = 2;
-    nFactors[9] = 2;
-    nFactors[10] = 4;
-    nFactors[11] = 2;
-    nFactors[12] = 1;
-    nFactors[13] = 4;
-    nFactors[14] = 3;
+    nFactors[9] = 4;
+    nFactors[10] = 2;
+    nFactors[11] = 1;
+    nFactors[12] = 4;
+    nFactors[13] = 3;
 
     }
 
