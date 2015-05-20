88081868fee4babb571f05282c0070312484f88d
Pascal-L <pascal.wieler@gmx.de>
2015-05-20 15:01:57 +0200

    Function Input - Einlesen der Funktionen klappt + Summenbildung

--- src/InfluenceFactors.java
+++ src/InfluenceFactors.java
@@ -12,38 +12,39 @@ public class InfluenceFactors {
     private Core _core;
 
 
-    public InfluenceFactors(Core pCore){
-        _core =pCore;
-    if(_core.loadConfig("InfluenceFactors.dat") == null) {
+    public InfluenceFactors(Core pCore) {
+        _core = pCore;
+        if (_core.loadConfig("InfluenceFactors.dat") == null) {
 
-    if(pCore.loadConfig("InfluenceFactors.dat") == null) {
 
-        nFactors[0] = 3;
-        nFactors[1] = 4;
-        nFactors[2] = 4;
-        nFactors[3] = 2;
-        nFactors[4] = 2;
-        nFactors[5] = 3;
-        nFactors[6] = 4;
-        nFactors[7] = 2;
-        nFactors[8] = 2;
-        nFactors[9] = 4;
-        nFactors[10] = 2;
-        nFactors[11] = 1;
-        nFactors[12] = 4;
-        nFactors[13] = 3;
 
-    DataKnot influenceFactors = new DataKnot("InfluenceFactors");
-        for (int i = 0; i < 14; i++){
-            influenceFactors.addData( "" + i, "" + nFactors[i]);
-        }
+                nFactors[0] = 3;
+                nFactors[1] = 4;
+                nFactors[2] = 4;
+                nFactors[3] = 2;
+                nFactors[4] = 2;
+                nFactors[5] = 3;
+                nFactors[6] = 4;
+                nFactors[7] = 2;
+                nFactors[8] = 2;
+                nFactors[9] = 4;
+                nFactors[10] = 2;
+                nFactors[11] = 1;
+                nFactors[12] = 4;
+                nFactors[13] = 3;
 
-        _core.saveConfig(influenceFactors, "InfluenceFactors.dat");
+                DataKnot influenceFactors = new DataKnot("InfluenceFactors");
+                for (int i = 0; i < 14; i++) {
+                    influenceFactors.addData("" + i, "" + nFactors[i]);
+                }
 
-    }else{
+                _core.saveConfig(influenceFactors, "InfluenceFactors.dat");
 
-        load();
-    }
+            } else {
+
+                load();
+            }
+
 
     }
 
